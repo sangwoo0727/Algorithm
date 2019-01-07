74196ebf60c9ee6ad619088db8d90661451cd551
sangwoo0727 <kangsw9395@gmail.com>
2019-01-07 17:56:28 +0900

    BOJ10039

--- BOJ10039.cpp
+++ BOJ10039.cpp
@@ -1,23 +1,27 @@
-/* 대상이 알려주기 위한 문제
-5명의 평균을 구하는 문제
-조건은 40점 미만인 학생은 40점으로 환산
-*/
+#include <stdio.h>
 
-#include <iostream>
-using namespace std;
+int main(void)
+{
+	int i = 0;
+	int sum = 0;
+	int grade[5];
 
-int arr[5];
-int main() {
-	int result = 0;
+	for (i = 0; i <= 5; i++)
+	{
 
-	for (int i = 0; i < 5; i++) {
-		scanf("%d", &arr[i]);
+		scanf("%d", &grade[i]);
+
+
+		if (grade[i] >= 40)
+		{
+			sum += grade[i];
+		}
+		else
+		{
+			sum += 40;
+		}
 	}
-	for (int i = 0; i < 5; i++) {
-		if (arr[i] <= 40)
-			arr[i] = 40;
-		result += arr[i];
-	}
-	printf("%d\n", result / 5);
+	printf("%d\n", sum / (i + 1));
+
 	return 0;
 }
