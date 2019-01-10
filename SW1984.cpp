db8e39f25f579f5f3480a4803afc8c6972d9e8b5
sangwoo0727 <kangsw9395@gmail.com>
2019-01-10 17:11:54 +0900

    SW1984

--- SW1984.cpp
+++ SW1984.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
+#include <algorithm>
+#include <math.h>
+
 using namespace std;
-long long dp[50];
 
-long long fib(int n) {
-	if (n == 0) return 0;
-	if (n == 1) return 1;
-	long long &ret = dp[n];
-	if (ret != -1) return ret;
-	ret = 0;
-	return ret = go(n - 1) + go(n - 2);
-}
-
-int main()[
-	int n = 0;
-	scanf("%d", &n);
-	memset(dp, -1, sizeof(dp));
-	printf("%lld", fib(n));
+int main() {
+	int T = 0;
+	int arr[10] = {};
+	double sum = 0;
+	int result;
+	scanf("%d", &T);
+	for (int i = 0; i < T; i++) {
+		for (int j = 0; j < 10; j++) {
+			scanf("%d", &arr[j]);
+		}
+		sort(arr, arr + 10);
+		for (int j = 1; j < 9; j++) {
+			sum += arr[j];
+		}
+		result = floor((sum / 8) + 0.5); //반올림하는 코드
+		printf("#%d %d\n", i + 1, result);
+		sum = 0;
+		result = 0;
+	}
 	return 0;
-]
+}
