ad78fa522bc1b0a8de0738c25f0fdfbc3161b3e0
sangwoo0727 <kangsw9395@gmail.com>
2019-02-12 18:02:51 +0900

    2743

---
+++ String/BOJ2743.cpp
@@ -0,0 +1,19 @@
+/*BOJ2743 단어길이재기
+입력받은 단어의 길이를 출력하는 문제
+C++에서 C라이브러리를 쓸때는 .h를 없애고 c를 붙히면된다.
+ex) #include <stdio.h> -> #include <cstdio>
+    #include <string.h> -> #include <cstring>
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+char str[105];
+int main() {
+	int num = 0;
+	scanf("%s", str);
+	num = strlen(str);
+	printf("%d\n", num);
+	return 0;
+}
