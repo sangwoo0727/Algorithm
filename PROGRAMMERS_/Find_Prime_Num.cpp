74dcd2ed077e8e156fbe4522ce681637371aaf4e
sangwoo0727 <kangsw9395@gmail.com>
2019-03-12 20:36:42 +0900

    0312

--- PROGRAMMERS_/Find_Prime_Num.cpp
+++ PROGRAMMERS_/Find_Prime_Num.cpp
@@ -18,7 +18,7 @@ int solution(int n) {
 	for (int i = 1; i <= n; i++) arr[i] = i;
 	for (int i = 2; i*i <= n; i++) {
 		if (arr[i] == 0) continue;
-		for (int j = i * i; j <= n; j += i) arr[j] = 0;
+		for (int j = i + i; j <= n; j += i) arr[j] = 0;
 	}
 	for (int i = 2; i <= n; i++) {
 		if (arr[i] != 0) answer++;
