1373866fe62937739b0aedc55e1905ddbc5a2ec4
sangwoo0727 <kangsw9395@gmail.com>
2019-04-01 01:52:58 +0900

    0401

---
+++ Simulation/BOJ1057.cpp
@@ -0,0 +1,17 @@
+/* BOJ 1057번 토너먼트*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, kim, lim;
+	cin >> N >> kim >> lim;
+	int cnt = 0;
+	while (kim != lim) {
+		kim = kim / 2 + kim % 2;
+		lim = lim / 2 + lim % 2;
+		cnt++;
+	}
+	cout << cnt << endl;
+	return 0;
+}
