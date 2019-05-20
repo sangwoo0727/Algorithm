066656d0abee0fe651b4476a40845cf28a7c3cbf
sangwoo0727 <kangsw9395@gmail.com>
2019-05-21 04:07:13 +0900

    0521

--- DataStructure/2.Linked_List/DummyLinkedlistSort.cpp
+++ DataStructure/2.Linked_List/DummyLinkedlistSort.cpp
@@ -4,27 +4,27 @@
 #define TRUE 1
 #define FALSE 0
 
-typedef struct _node {
+typedef struct _node { //노드구조체
 	int data;
 	struct _node *next;
 }Node;
 
-typedef struct {
+typedef struct { //리스트를 의미하는 구조체
 	Node * head;
 	Node * cur;
 	Node * before;
 	int numOfData;
-	int(*comp)(int d1, int d2);
+	int(*comp)(int d1, int d2); //함수 포인터 변수
 }List;
 
-void ListInit(List * plist) {
+void ListInit(List * plist) { //리스트 처음 만들때 초기화시켜서 공간 할당받는 역할
 	plist->head = (Node*)malloc(sizeof(Node));
 	plist->head->next = NULL;
 	plist->comp = NULL;
 	plist->numOfData = 0;
 }
 
-void LInsert(List * plist, int data) {
+void LInsert(List * plist, int data) { //데이터 삽입시 정렬함수를 사용하여 정렬하며 리스트에 데이터 삽입
 	Node * newNode = (Node*)malloc(sizeof(Node));
 	Node * pred = plist->head;
 	newNode->data = data;
@@ -35,7 +35,7 @@ void LInsert(List * plist, int data) {
 	(plist->numOfData)++;
 }
 
-int LFirst(List *plist, int *pdata) {
+int LFirst(List *plist, int *pdata) { //리스트의 첫번째 노드의 데이터 값 저장 및 첫번째 노드 존재하는지에 대한 참/거짓반환
 	if (plist->head->next == NULL) return FALSE;
 	plist->before = plist->head;
 	plist->cur = plist->head->next;
@@ -43,7 +43,7 @@ int LFirst(List *plist, int *pdata) {
 	return TRUE;
 }
 
-int LNext(List *plist, int *pdata) {
+int LNext(List *plist, int *pdata) { //리스트의 두번째 이후 노드부터 값 저장 및 반환
 	if (plist->cur->next == NULL) return FALSE;
 	plist->before = plist->cur;
 	plist->cur = plist->cur->next;
@@ -52,7 +52,7 @@ int LNext(List *plist, int *pdata) {
 	return TRUE;
 }
 
-int LRemove(List *plist) {
+int LRemove(List *plist)  //노드 삭제
 	Node * rpos = plist->cur;
 	int rdata = rpos->data;
 	plist->before->next = plist->cur->next;
@@ -62,11 +62,11 @@ int LRemove(List *plist) {
 	return rdata;
 }
 
-int LCount(List *plist) {
+int LCount(List *plist) { //현재 리스트에 존재하는 노드 개수 반환
 	return plist->numOfData;
 }
 
-int WhoIsPrecede(int d1, int d2) {
+int WhoIsPrecede(int d1, int d2) { //정렬하기 위해 사용한 함수
 	if (d1 < d2) return 0;
 	else return 1;
 }
