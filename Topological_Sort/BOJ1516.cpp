842af0d90cf6acc92eff9235f884b3263e1d5e8f
sangwoo0727 <kangsw9395@gmail.com>
2019-06-07 18:31:58 +0900

    0607

--- Topological_Sort/BOJ1516.cpp
+++ Topological_Sort/BOJ1516.cpp
@@ -9,19 +9,17 @@ queue <int> q;
 int weight[505];
 int ind[505];
 int dp[505];
-int check[505];
 
 void bfs() {
 	while (!q.empty()) {
 		int now = q.front();
-		check[now] = 1;
 		q.pop();
 		int size = adj[now].size();
 		for (int i = 0; i < size; i++) {
 			int next = adj[now][i];
 			ind[next]--;
 			dp[next] = max(dp[next], dp[now] + weight[next]);
-			if (check[next] == 0 && ind[next] == 0)
+			if (ind[next] == 0)
 				q.push(next);
 		}
 	}
