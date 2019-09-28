eb4310909e75c8d594745f4dc6bea26f4ffb81e5
sangwoo0727 <kangsw9395@gmail.com>
2019-09-28 13:57:37 +0900

    Create BOJ9935.cpp

---
+++ String/BOJ9935.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <deque>
+#include <stack>
+using namespace std;
+
+int main() {
+	deque <char> store;
+	string s, bs;
+	cin >> s;
+	cin >> bs;
+	for (const auto& c : s) {
+		store.push_back(c);
+		if (c == bs[bs.length() - 1]) {
+			stack <char> tmp;
+			for (int i = bs.length() - 1; i >= 0; i--) {
+				if (!store.empty() && store.back() == bs[i]) {
+					tmp.push(store.back());
+					store.pop_back();
+				}
+				if (i == 0 && tmp.size() != bs.length()) {
+					while (!tmp.empty()) {
+						store.push_back(tmp.top());
+						tmp.pop();
+					}
+				}
+			}
+		}
+	}
+	if (store.empty()) cout << "FRULA\n";
+	else {
+		while (!store.empty()) {
+			cout << store.front();
+			store.pop_front();
+		}
+		cout << "\n";
+	}
+	return 0;
+}
