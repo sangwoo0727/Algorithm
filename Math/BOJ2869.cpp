920aa5fa6bb466bc9f86a3ec54221209de0662f7
sangwoo0727 <kangsw9395@gmail.com>
2019-09-28 12:27:43 +0900

    Update BOJ2869.cpp

--- Math/BOJ2869.cpp
+++ Math/BOJ2869.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
 	int a, b, v;
 	cin >> a >> b >> v;
-	int ans = (v - a) % (a - b) == 0 ? ans = 1 + (v - a) / (a - b) : ans = 2 + (v - a) / (a - b);
-	cout << ans;
+	(v - a) % (a - b) == 0 ? cout << 1 + (v - a) / (a - b) : cout << 2 + (v - a) / (a - b);
 	return 0;
 }
