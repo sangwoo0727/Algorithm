4045263cd8f093842528f7fa7cdb8c4670bc91a8
sangwoo0727 <kangsw9395@gmail.com>
2019-11-21 17:23:38 +0900

    Create Programmers_프린터.cpp

---
+++ BruteForce/Programmers_프린터.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <functional>
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+	int answer = 0;
+	priority_queue <int, vector<int>, less<int>> pq;
+	for (int i = 0; i < priorities.size(); i++) {
+		pq.push(priorities[i]);
+	}
+	while (priorities.size()) {
+		if (priorities[0] < pq.top()) {
+			int num = priorities[0];
+			priorities.push_back(num);
+			priorities.erase(priorities.begin());
+			if (location == 0) {
+				location = priorities.size() - 1;
+			}
+			else location--;
+		}
+		if (priorities[0] == pq.top()) {
+			if (location == 0) {
+				priorities.erase(priorities.begin());
+				answer++;
+				break;
+			}
+			answer++;
+			location--;
+			priorities.erase(priorities.begin());
+			pq.pop();
+		}
+	}
+	return answer;
+}
