c0285ee52406352fd2b8d1664985fb6e1d0bfa8b
SangWoo Kang <kangsw9395@gmail.com>
2020-03-12 15:26:45 +0900

    Update SWEA7701_염라대왕의이름정렬.java

--- String/SWEA7701_염라대왕의이름정렬.java
+++ String/SWEA7701_염라대왕의이름정렬.java
@@ -5,32 +5,25 @@ import java.io.*;
 public class SWEA7701_염라대왕의이름정렬 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
-	static PriorityQueue<String> pq;
-	static HashSet<String> set;
+	static TreeSet<String> ts;
 	static int N;
 	
     public static void main(String[] args) throws IOException {
     	int tc = Integer.parseInt(br.readLine());
     	for(int t=1; t<=tc; t++) {
     		sb.append("#").append(t).append("\n");
-    		pq = new PriorityQueue<>(new Comparator<String>() {
-				@Override
+    		N = Integer.parseInt(br.readLine());
+    		ts = new TreeSet<>(new Comparator<String>() {
 				public int compare(String o1, String o2) {
 					int t = o1.length()-o2.length();
 					if(t==0) return o1.compareTo(o2);
 					else return t;
 				}
-    		});
-    		set = new HashSet<>();
-    		N = Integer.parseInt(br.readLine());
-    		while(N-->0) {
-    			String s = br.readLine();
-    			if(!set.contains(s)) {
-    				set.add(s);
-    				pq.offer(s);
-    			}
+			});
+    		for(int i=0; i<N; i++) {
+    			ts.add(br.readLine());
     		}
-    		while(!pq.isEmpty()) sb.append(pq.poll()).append("\n");
+    		while(!ts.isEmpty()) sb.append(ts.pollFirst()).append("\n");
     	}
     	System.out.println(sb);
     }
