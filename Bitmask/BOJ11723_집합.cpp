8591422a4170afff5bda20ffd8913da02e476e77
SangWoo Kang <kangsw9395@gmail.com>
2020-03-22 15:37:25 +0900

    Update BOJ11723_집합.cpp

--- Bitmask/BOJ11723_집합.cpp
+++ Bitmask/BOJ11723_집합.cpp
@@ -1,43 +1,40 @@
 #include <iostream>
 #include <string>
-#define endl '\n'
-
+#define endl "\n";
 using namespace std;
 
+int M;
+int set = 0;
+
 int main(){
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int N=0,M=0,num=0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     cin >> M;
+
     while(M--){
         string s;
+        int n;
         cin >> s;
-        if(s == "add"){
-            cin >> num;
-            N |= (1<<num);
-        }
-        else if(s=="remove"){
-            cin >> num;
-            N &= ~(1<<num);
-        }
-        else if(s=="check"){
-            cin >> num;
-            int tmp = (1<<num);
-            if(N & tmp) {
-                cout << "1"<<endl;
+        if(s=="add"){
+            cin >> n;
+            set |= (1<<n);
+        }else if(s=="check"){
+            cin >> n;
+            if(set & (1<<n)){
+                cout << "1"<< endl;
+            }else {
+                cout << "0" << endl;
             }
-            else {
-                cout << "0"<<endl;
-            }
-        }
-        else if(s=="toggle"){
-            cin >> num;
-            N ^= (1<<num);
-        }
-        else if(s=="all"){
-            N ^= (1<<21)-1;
-        }
-        else if(s=="empty"){
-            N = 0;
+        }else if(s=="remove"){
+            cin >> n;
+            set &= ~(1<<n);
+        }else if(s=="toggle"){
+            cin >> n;
+            set ^= (1<<n);
+        }else if(s=="empty"){
+            set = 0;
+        }else if(s=="all"){
+            set = (1<<21)-1;
         }
     }
     return 0;
