fbd2fe0daaacadaeebb32008ace83f5ec5108753
SangWoo Kang <kangsw9395@gmail.com>
2020-04-04 01:31:49 +0900

    Create BOJ12015_가장긴증가하는부분순열2.java

---
+++ LIS/BOJ12015_가장긴증가하는부분순열2.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ12015_가장긴증가하는부분순열2 {
+	static int N;
+	static int[] arr;
+	static int[] lis;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N+1];
+		lis = new int[N+1];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		lis[0]=arr[0];
+		int idx = 0;
+		for(int i=1; i<N; i++) {
+			if(lis[idx]<arr[i]) {
+				lis[++idx] = arr[i];
+			}else {
+				int ii = lower_bound(0,idx,arr[i]);
+				lis[ii] = arr[i];
+			}
+		}
+		System.out.println(idx+1);
+	}
+	static int lower_bound(int l, int r, int k) {
+		int m;
+		while(l<r) {
+			m = (l+r)/2;
+			if(lis[m] < k) l = m+1;
+			else r = m;
+		}
+		return r;
+	}	
+}
