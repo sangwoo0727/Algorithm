370d71d0af864d5c7c9d5d29470ac1e633982fd7
SangWoo Kang <kangsw9395@gmail.com>
2020-08-07 20:05:32 +0900

    Update Kakao_2019BlindRecruitment_3_후보키.java

--- 카카오/Kakao_2019BlindRecruitment_3_후보키.java
+++ 카카오/Kakao_2019BlindRecruitment_3_후보키.java
@@ -1,51 +1,37 @@
 import java.util.*;
-
 public class Kakao_2019BlindRecruitment_3_후보키 {
-	static HashSet<Integer> set = new HashSet<>();
-	static List<Integer> list = new ArrayList<>();
-	static HashSet<String> sset;
-	static int N,M;
-	public int solution(String[][] relation) {
-		N = relation.length;
-		M = relation[0].length;
-		for(int i=1; i<(1<<M); i++) {
-			list.add(i);
-		}
-		Collections.sort(list, (Integer o1, Integer o2)->{
-			int cnt = 0, cnt2 = 0;
-			for(int i=0; i<M; i++) {
-				if((o1 &(1<<i))>0) cnt++;
-				if((o2 &(1<<i))>0) cnt2++;
-			}
-			if(cnt==cnt2) return o1.compareTo(o2);
-			else return cnt-cnt2;
-		});
-		for(int bit: list) {
-			boolean flg = false;
-			for(int n:set) {
-				int tmp = n|bit;
-				int cnt=0, cnt2=0;
-				for(int i=0; i<M; i++) {
-					if((bit &(1<<i))>0) cnt++;
-					if((tmp &(1<<i))>0) cnt2++;
-				}
-				if(cnt==cnt2) flg = true;
-			}
-			if(flg) continue;
-			sset = new HashSet<>();
-			for(int i=0; i<N; i++) {
-				StringBuilder sb = new StringBuilder();
-				for(int j=0; j<M; j++) {
-					if((bit&(1<<j))>0) {
-						sb.append(relation[i][j]).append(" ");
-					}
-				}
-				sset.add(sb.toString());
-			}
-			if(sset.size()==N) {
-				set.add(bit);
-			}
-		}
-		return set.size();
-	}
+    static List<Integer> bList = new ArrayList<>();
+    public static int solution(String[][] relation){
+        int N = relation.length;
+        int M = relation[0].length;
+        for(int bit=1; bit<(1<<M); bit++){
+            bList.add(bit);
+        }
+        bList.sort((o1, o2) -> {
+            int cnt1 = 0, cnt2 = 0;
+            for(int i=0; i<M; i++){
+                if((o1&(1<<i))>0) cnt1++;
+                if((o2&(1<<i))>0) cnt2++;
+            }
+            return cnt1 == cnt2 ? o1.compareTo(o2) : Integer.compare(cnt1,cnt2);
+        });
+        List<Integer> list = new ArrayList<>();
+        label : for(Integer bit : bList){
+            for(Integer n : list){
+                if((bit&n) == n){
+                    continue label;
+                }
+            }
+            Set<String> set = new HashSet<>();
+            for (String[] string : relation) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < M; i++) {
+                    if ((bit & (1 << i)) > 0) sb.append(string[i]).append(" ");
+                }
+                set.add(sb.toString());
+            }
+            if(set.size() == N) list.add(bit);
+        }
+        return list.size();
+    }
 }
