b11f1604f814b087517283ebbbf53137df293784
sangwoo0727 <kangsw9395@gmail.com>
2020-10-27 08:41:42 +0900

    Refactoring LeetCode_5 Longest Palindromic Substring

--- LeetCode/LeetCode_5_LongestPalindrome.py
+++ LeetCode/LeetCode_5_LongestPalindrome.py
@@ -1,29 +1,15 @@
+from typing import *
+
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) <= 1:
-            return s
-
-        dp: list = [[0] * len(s) for _ in range(len(s))]
+        dp: List[List[bool]] = [[False] * len(s) for _ in range(len(s))]
         answer: str = s[0]
 
-        for idx in range(len(s)):
-            dp[idx][idx] = 1
-
-        for idx in range(len(s) - 1):
-            if s[idx] == s[idx + 1]:
-                dp[idx][idx + 1] = 1
-                answer = s[idx:idx + 2]
-            else:
-                dp[idx][idx + 1] = 0
-
-        for length in range(3, len(s) + 1):
-            for idx in range(len(s) - length + 1):
-                right_idx: int = idx + length - 1
-                if s[idx] == s[right_idx] and dp[idx + 1][right_idx - 1] == 1:
-                    dp[idx][right_idx] = 1
-                    if len(answer) < length:
-                        answer = s[idx:right_idx + 1]
-                else:
-                    dp[idx][right_idx] = 0
+        for idx in reversed(range(len(s))):
+            for jdx in range(idx, len(s)):
+                dp[idx][jdx] = s[idx] == s[jdx] and (jdx - idx <= 2 or dp[idx + 1][jdx - 1])
+                if dp[idx][jdx] and jdx - idx >= len(answer):
+                    answer = s[idx:jdx + 1]
 
         return answer
