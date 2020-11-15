23a639d049032f7441ab2e23acb67954538db400
sangwoo0727 <kangsw9395@gmail.com>
2020-11-15 17:51:31 +0900

    Create LeetCode 22 Generate Parentheses

---
+++ LeetCode/LeetCode22_GenerateParentheses.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class LeetCode22_GenerateParentheses {
+    static List<String> output;
+    static Deque<Character> stack;
+    static StringBuilder sb;
+    public List<String> generateParenthesis(int n) {
+        output = new ArrayList<>();
+        stack = new ArrayDeque<>();
+        sb = new StringBuilder();
+        comb(0,0,n);
+        return output;
+    }
+    static void comb(int left,int right, int n){
+        if(left == n && right == n){
+            output.add(sb.toString());
+            return;
+        }
+        if(left < n){
+            stack.add('(');
+            sb.append('(');
+            comb(left+1, right, n);
+            stack.poll();
+            sb.deleteCharAt(sb.length()-1);
+        }
+        if(right < n){
+            if(stack.size()> right){
+                sb.append(')');
+                comb(left,right+1,n);
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+    }
+}
