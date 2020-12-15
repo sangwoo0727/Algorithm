a2e3e6213141f88b7e8291cfce89d0bf57081036
sangwoo0727 <kangsw9395@gmail.com>
2020-12-15 10:17:11 +0900

    Update BOJ20159 동작그만.밑장빼기냐?

--- 누적합/BOJ20159_동작그만밑장빼기냐.java
+++ 누적합/BOJ20159_동작그만밑장빼기냐.java
@@ -3,40 +3,31 @@ import java.util.*;
 
 public class BOJ20159_동작그만밑장빼기냐 {
     static int N, output;
-    static int[] input, psum;
+    static int[] input, myPsum, yourPsum;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        input = new int[N];
-        psum = new int[N];
+        input = new int[N + 1];
+        myPsum = new int[N + 1];
+        yourPsum = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             int n = Integer.parseInt(st.nextToken());
             input[i] = n;
-            if (i == N - 1){
-                psum[i] = i < 2 ? 0 : psum[i - 2];
-                break;
-            }
-            if (i == 0 || i == 1) {
-                psum[i] = n;
+            if (i % 2 == 0) { // your
+                yourPsum[i] = yourPsum[i - 1] + n;
+                myPsum[i] = myPsum[i - 1];
             } else {
-                psum[i] = psum[i - 2] + n;
+                yourPsum[i] = yourPsum[i - 1];
+                myPsum[i] = myPsum[i - 1] + n;
             }
         }
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             int sum;
-            if (i % 2 == 0) {
-                if (i - 2 >= 0) {
-                    sum = psum[i - 2] + input[N - 1] + (psum[N - 1] - psum[i - 1]);
-                } else {
-                    sum = input[N - 1] + psum[N - 1];
-                }
-            } else {
-                if (i - 2 >= 0) {
-                    sum = psum[i - 1] + (psum[N - 1] - psum[i - 2]);
-                } else {
-                    sum = psum[i - 1] + psum[N - 1];
-                }
+            if (i % 2 == 0) { // trick for you
+                sum = myPsum[i - 1] + yourPsum[N] - yourPsum[i - 1] - input[N];
+            } else { // trick for me
+                sum = myPsum[i - 1] + yourPsum[N] - yourPsum[i];
             }
             output = Math.max(output, sum);
         }
