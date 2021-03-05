0977c3067bab4ecc800de4aaf6166b71b56215e7
sangwoo0727 <kangsw9395@gmail.com>
2021-03-06 00:13:23 +0900

    프로그래머스 JadenCase 문자열만들기

---
+++ 프로그래머스/LEV2_JadenCase문자열만들기.java
@@ -0,0 +1,26 @@
+public class LEV2_JadenCase문자열만들기 {
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                answer.append(c);
+                continue;
+            }
+            if (i == 0 || s.charAt(i - 1) == ' ') {
+                if (c >= 'a' && c <= 'z') {
+                    answer.append((char) (c - 32));
+                } else {
+                    answer.append(c);
+                }
+            } else {
+                if (c >= 'A' && c <= 'Z') {
+                    answer.append((char) (c + 32));
+                } else {
+                    answer.append(c);
+                }
+            }
+        }
+        return answer.toString();
+    }
+}
