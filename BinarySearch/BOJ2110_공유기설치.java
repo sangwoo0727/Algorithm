502ff291808af7124c20fc29b846ccc699872b67
sangwoo0727 <kangsw9395@gmail.com>
2021-04-19 22:03:30 +0900

    BOJ2110 공유기설치

--- BinarySearch/BOJ2110_공유기설치.java
+++ BinarySearch/BOJ2110_공유기설치.java
@@ -1,46 +1,39 @@
 import java.io.*;
 import java.util.*;
+
 public class BOJ2110_공유기설치 {
-	static int N,C;
-	static int arr[];
-	static int ans;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		Arrays.sort(arr);
-		bs(1,arr[N-1]-arr[0]);
-		System.out.println(ans);
-	}
-	static void bs(int l, int r) {
-		int m=0;
-		while(l<=r) {
-			m = (l+r)/2;
-			int tmp = arr[0];
-			int cnt = 1;
-			boolean flg = false;
-			for(int i=1; i<N; i++) {
-				if(arr[i]-tmp < m) continue;
-				else if(arr[i]-tmp == m) {
-					flg = true;
-					cnt++;
-					tmp = arr[i];
-				}else if(arr[i]-tmp > m) {
-					cnt++;
-					tmp = arr[i];
-				}
-			}
-			if(cnt < C) {
-				r = m-1;
-			}else {
-				if(flg) ans=m;
-				l = m+1;
-			}
-		}
-	}
+    private static int N, C;
+    private static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(input.readLine());
+        }
+        Arrays.sort(arr);
+        int ans = 0;
+        int l = 1;
+        int r = arr[N - 1] - arr[0];
+        while (l <= r) {
+            int m = (l + r) / 2;
+            int cnt = 1;
+            int prev = arr[0];
+            for (int i = 1; i < N; i++) {
+                if (arr[i] - prev >= m) {
+                    cnt++;
+                    prev = arr[i];
+                }
+            }
+            if (cnt < C) {
+                r = m - 1;
+            } else {
+                ans = m;
+                l = m + 1;
+            }
+        }
+        System.out.println(ans);
+    }
 }
