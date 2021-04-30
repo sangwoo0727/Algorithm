5be88c0e1a71c2506f67cdd666cc9b2068443375
sangwoo0727 <kangsw9395@gmail.com>
2021-05-01 01:27:16 +0900

    LeetCode 11 Container With Most Water

---
+++ LeetCode🥇/LeetCode_11_ContainerWithMostWater.java
@@ -0,0 +1,13 @@
+public class LeetCode_11_ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int ans = 0;
+        while (l <= r) {
+            int h = Math.min(height[l], height[r]);
+            ans = Math.max(ans, h * (r - l));
+            if (height[l] < height[r]) l++;
+            else r--;
+        }
+        return ans;
+    }
+}
