72eb9f2f8c42f5f451c7ff683aba120b2e1851ea
sangwoo0727 <kangsw9395@gmail.com>
2021-05-25 20:48:53 +0900

    백준 15652 N과M 4

---
+++ BruteForce/BOJ15652_N과M4_210525.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15652_N과M4_210525 {
+    private static int N, M;
+    private static int[] selected;
+    private static StringBuilder output = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        recFunc(1);
+        System.out.println(output);
+    }
+
+    private static void recFunc(int k) {
+        if (k == M + 1) {
+            for (int m = 1; m <= M; m++) {
+                output.append(selected[m]).append(" ");
+            }
+            output.append("\n");
+            return;
+        }
+        int start = selected[k - 1];
+        if (start == 0) start = 1;
+        for (int n = start; n <= N; n++) {
+            selected[k] = n;
+            recFunc(k + 1);
+        }
+    }
+
+    private static void input() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        selected = new int[M + 1];
+    }
+}
