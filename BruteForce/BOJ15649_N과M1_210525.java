c49d1c4fed90eedb8fa192da297c17f235b36ea7
sangwoo0727 <kangsw9395@gmail.com>
2021-05-25 19:44:35 +0900

    백준 15649 N과M 1

---
+++ BruteForce/BOJ15649_N과M1_210525.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15649_N과M1_210525 {
+    private static int N, M;
+    private static int[] selected;
+    private static StringBuilder output = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        recFunc(1);
+        System.out.println(output);
+    }
+
+    private static void recFunc(int k) {
+        if (k == M + 1) {
+            for (int m = 1; m <= M; m++) {
+                output.append(selected[m]).append(" ");
+            }
+            output.append("\n");
+            return;
+        }
+        for (int n = 1; n <= N; n++) {
+            boolean isUsed = false;
+            for (int i = 1; i < k; i++) {
+                if (n == selected[i]) {
+                    isUsed = true;
+                    break;
+                }
+            }
+            if (!isUsed) {
+                selected[k] = n;
+                recFunc(k + 1);
+            }
+        }
+    }
+    private static void input() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        selected = new int[M + 1];
+    }
+}
