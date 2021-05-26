53e3d696d9b3a336abba108913bdc98aab76c7d8
sangwoo0727 <kangsw9395@gmail.com>
2021-05-26 19:16:13 +0900

    백준 14888 연산자 끼워넣기

--- 백트래킹/BOJ14888_연산자끼워넣기_210526.java
+++ 백트래킹/BOJ14888_연산자끼워넣기_210526.java
@@ -1,52 +1,42 @@
 import java.io.*;
 import java.util.*;
 
-public class BOJ14888_210526 {
+public class BOJ14888_연산자끼워넣기_210526 {
     private static int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
     private static int N;
     private static int[] nums;
     private static int[] opers;
-    private static int[] selected;
     public static void main(String[] args) throws IOException {
         input();
-        recFunc(1);
+        recFunc(1, nums[0]);
 
         System.out.println(max);
         System.out.println(min);
     }
 
-    private static int calc() {
-        int total = nums[0];
-        for (int i = 1; i < N; i++) {
-            if (selected[i] == 0) {
-                total += nums[i];
-            } else if (selected[i] == 1) {
-                total -= nums[i];
-            } else if (selected[i] == 2) {
-                total *= nums[i];
-            } else {
-                if (total < 0) {
-                    total = (Math.abs(total) / nums[i]) * -1;
-                } else {
-                    total /= nums[i];
-                }
-            }
+    private static int calc(int value, int oper, int next) {
+        if (oper == 0) {
+            value += next;
+        } else if (oper == 1) {
+            value -= next;
+        } else if (oper == 2) {
+            value *= next;
+        } else {
+            value /= next;
         }
-        return total;
+        return value;
     }
 
-    private static void recFunc(int k) {
+    private static void recFunc(int k, int value) {
         if (k == N) {
-            int total = calc();
-            max = Math.max(max, total);
-            min = Math.min(min, total);
+            max = Math.max(max, value);
+            min = Math.min(min, value);
             return;
         }
         for (int i = 0; i < 4; i++) {
             if (opers[i] >= 1) {
                 opers[i]--;
-                selected[k] = i;
-                recFunc(k + 1);
+                recFunc(k + 1, calc(value, i, nums[k]));
                 opers[i]++;
             }
         }
@@ -56,7 +46,6 @@ public class BOJ14888_210526 {
         N = Integer.parseInt(input.readLine());
         StringTokenizer st = new StringTokenizer(input.readLine());
         nums = new int[N];
-        selected = new int[N];
         opers = new int[4];
         for (int i = 0; i < N; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
